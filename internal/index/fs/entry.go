dcfad33059a609e63bb447bea64c612301e6e043
skewb1k <skewb1kunix@gmail.com>
2025-06-08 14:41:08 +0300

    storage improvements

--- internal/index/fs/entry.go
+++ internal/index/fs/entry.go
@@ -3,9 +3,6 @@ package indexFs
 import (
 	"context"
 	"encoding/base64"
-	"errors"
-	"fmt"
-	"os"
 	"path/filepath"
 
 	"github.com/skewb1k/upfile/internal/index"
@@ -13,7 +10,7 @@ import (
 
 const (
 	entriesDirname = "entries"
-	byDirName      = "by-dir"
+	byEntry        = "by-entry"
 	byName         = "by-name"
 )
 
@@ -26,90 +23,77 @@ func (p Provider) getPathToEntriesByName(fname string) string {
 	)
 }
 
-func (p Provider) getPathToEntriesByDir(dir string) string {
+func (p Provider) getPathToFilenamesByEntries(entry string) string {
 	return filepath.Join(
 		p.BaseDir,
 		entriesDirname,
-		byDirName,
-		filepath.Clean(dir),
+		byEntry,
+		filepath.Clean(entry),
 	)
 }
 
-func encodePath(path []byte) string {
-	return base64.URLEncoding.EncodeToString(path)
+func encodePath(path string) string {
+	return base64.URLEncoding.EncodeToString([]byte(path))
 }
 
-func decodePath(encoded string) ([]byte, error) {
-	res, err := base64.URLEncoding.DecodeString(encoded)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode base64: %w", err)
-	}
-
-	return res, nil
-}
+//	func decodePath(encoded string) (string, error) {
+//		res, err := base64.URLEncoding.DecodeString(encoded)
+//		if err != nil {
+//			return "", fmt.Errorf("failed to decode base64: %w", err)
+//		}
+//
+//		return string(res), nil
+//	}
 
 func (p Provider) CreateEntry(
 	ctx context.Context,
 	fname string,
 	entry string,
 ) error {
-	encoded := encodePath([]byte(entry))
+	encoded := encodePath(entry)
 
-	byDirPath := p.getPathToEntriesByDir(encoded)
-	if err := os.MkdirAll(byDirPath, 0o700); err != nil {
-		return fmt.Errorf("mkdir by-dir path: %w", err)
-	}
+	byEntryPath := p.getPathToFilenamesByEntries(encoded)
 
-	byDirFile, err := os.OpenFile(filepath.Join(byDirPath, fname), os.O_CREATE|os.O_EXCL, 0o600)
+	bydir, err := Load(byEntryPath)
 	if err != nil {
-		if errors.Is(err, os.ErrExist) {
-			return index.ErrExists
-		}
-
-		return fmt.Errorf("create by-dir entry: %w", err)
-	}
-	_ = byDirFile.Close()
-
-	byNameDir := p.getPathToEntriesByName(fname)
-
-	if err := os.MkdirAll(byNameDir, 0o700); err != nil {
-		return fmt.Errorf("mkdir by-name dir: %w", err)
+		return err
 	}
 
-	byNameFile, err := os.OpenFile(filepath.Join(byNameDir, encoded), os.O_CREATE|os.O_EXCL, 0o600)
+	if !bydir.Add(fname) {
+		return index.ErrExists
+	}
+
+	byNamePath := p.getPathToEntriesByName(fname)
+
+	byname, err := Load(byNamePath)
 	if err != nil {
-		if errors.Is(err, os.ErrExist) {
-			return index.ErrExists
-		}
-
-		return fmt.Errorf("create by-name entry: %w", err)
+		return err
 	}
-	_ = byNameFile.Close()
+
+	if !byname.Add(entry) {
+		return index.ErrExists
+	}
+
+	if err := byname.Save(byNamePath); err != nil {
+		return err
+	}
+
+	if err := bydir.Save(byEntryPath); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func (p Provider) GetEntriesByFilename(ctx context.Context, fname string) ([]string, error) {
-	entries, err := os.ReadDir(p.getPathToEntriesByName(fname))
+	byNamePath := p.getPathToEntriesByName(fname)
+
+	byname, err := Load(byNamePath)
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return nil, nil
-		}
-
-		return nil, fmt.Errorf("read by-name path: %w", err)
+		return nil, err
 	}
 
-	result := make([]string, len(entries))
-	for i, entry := range entries {
-		e, err := decodePath(entry.Name())
-		if err != nil {
-			return nil, err
-		}
-
-		result[i] = string(e)
-	}
-
-	return result, nil
+	return byname.ToSlice(), nil
 }
 
 func (p Provider) DeleteEntry(
@@ -117,72 +101,59 @@ func (p Provider) DeleteEntry(
 	fname string,
 	entry string,
 ) error {
-	encoded := encodePath([]byte(entry))
-	byDirDir := p.getPathToEntriesByDir(encoded)
+	encoded := encodePath(entry)
 
-	if err := os.Remove(filepath.Join(byDirDir, fname)); err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			err = index.ErrNotFound
-		}
+	byEntryPath := p.getPathToFilenamesByEntries(encoded)
 
-		return fmt.Errorf("remove by-dir entry file: %w", err)
+	bydir, err := Load(byEntryPath)
+	if err != nil {
+		return err
 	}
 
-	if entries, err := os.ReadDir(byDirDir); err == nil && len(entries) == 0 {
-		if err := os.Remove(byDirDir); err != nil {
-			return fmt.Errorf("remove by-dir entry dir: %w", err)
-		}
+	if !bydir.Delete(fname) {
+		return index.ErrNotFound
 	}
 
-	byNameDir := p.getPathToEntriesByName(fname)
-	if err := os.Remove(filepath.Join(byNameDir, encoded)); err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			err = index.ErrNotFound
-		}
+	byNamePath := p.getPathToEntriesByName(fname)
 
-		return fmt.Errorf("remove by-name entry file: %w", err)
+	byname, err := Load(byNamePath)
+	if err != nil {
+		return err
 	}
 
-	if entries, err := os.ReadDir(byNameDir); err == nil && len(entries) == 0 {
-		if err := os.Remove(byNameDir); err != nil {
-			return fmt.Errorf("remove by-name entry dir: %w", err)
-		}
+	if !byname.Delete(entry) {
+		return index.ErrNotFound
+	}
+
+	if err := byname.Save(byNamePath); err != nil {
+		return err
+	}
+
+	if err := bydir.Save(byEntryPath); err != nil {
+		return err
 	}
 
 	return nil
 }
 
 func (p Provider) CheckEntry(ctx context.Context, fname string, entry string) (bool, error) {
-	byNamePath := filepath.Join(p.getPathToEntriesByName(fname), encodePath([]byte(entry)))
-
-	if _, err := os.Stat(byNamePath); err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return false, nil
-		}
-
-		return false, fmt.Errorf("stat by-name file: %w", err)
+	byname, err := Load(p.getPathToEntriesByName(fname))
+	if err != nil {
+		return false, err
 	}
 
-	return true, nil
+	_, exists := byname[entry]
+	return exists, nil
 }
 
 func (p Provider) GetFilenamesByEntry(ctx context.Context, entry string) ([]string, error) {
-	encoded := encodePath([]byte(entry))
-	byDirDir := p.getPathToEntriesByDir(encoded)
+	encoded := encodePath(entry)
+	byEntryPath := p.getPathToFilenamesByEntries(encoded)
 
-	entries, err := os.ReadDir(byDirDir)
+	bydir, err := Load(byEntryPath)
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return nil, nil
-		}
-
-		return nil, fmt.Errorf("read versions dir: %w", err)
+		return nil, err
 	}
 
-	dirs := make([]string, len(entries))
-	for i, entry := range entries {
-		dirs[i] = entry.Name()
-	}
-
-	return dirs, nil
+	return bydir.ToSlice(), nil
 }
---
+++ internal/index/fs/entry_set.go
@@ -0,0 +1,91 @@
+package indexFs
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"maps"
+	"os"
+	"path/filepath"
+	"slices"
+)
+
+type EntrySet map[string]struct{}
+
+func (e EntrySet) Add(s string) bool {
+	if _, exists := e[s]; exists {
+		return false
+	}
+
+	e[s] = struct{}{}
+	return true
+}
+
+func (e EntrySet) Delete(s string) bool {
+	if _, exists := e[s]; !exists {
+		return false
+	}
+
+	delete(e, s)
+	return true
+}
+
+// TODO: improve performance
+func (e EntrySet) ToSlice() []string {
+	return slices.Sorted(maps.Keys(e))
+}
+
+func (e EntrySet) Save(path string) error {
+	if len(e) == 0 {
+		if err := os.Remove(path); err != nil && !errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("remove file: %w", err)
+		}
+
+		return nil
+	}
+
+	if err := os.MkdirAll(filepath.Dir(path), 0o700); err != nil {
+		return fmt.Errorf("mkdir: %w", err)
+	}
+
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+	defer f.Close()
+
+	for _, item := range e.ToSlice() {
+		if _, err := f.WriteString(item + "\n"); err != nil {
+			return fmt.Errorf("write string: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func Load(path string) (EntrySet, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return make(EntrySet), nil
+		}
+
+		return nil, fmt.Errorf("open file: %w", err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	set := make(EntrySet)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			set[line] = struct{}{}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner: %w", err)
+	}
+
+	return set, nil
+}
